215c987bc3f54c3b9cbfdc27185fcd2192c8c5e7
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-06-13 13:38:54 +0530

    sourceFile splited into single files

--- pdfsplit.py
+++ pdfsplit.py
@@ -4,10 +4,11 @@ try:
     doc = fitz.open(file) 
 except Exception as e:
     print(e)
-#page_count  = doc.pageCount
-for page in doc:   
+
+for page in doc:
+    
     doc2 =fitz.open()
-    doc2.insert_pdf(doc)
+    doc2.insert_pdf(doc,from_page=page.number,to_page=page.number)
     doc2.save("data/physicsnobel-%i.pdf"%page.number)
     
     
