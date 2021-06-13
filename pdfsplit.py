456e789ab43be0073961742bc231e2d2b48af36f
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-06-13 11:46:22 +0530

    individual pages-test

--- pdfsplit.py
+++ pdfsplit.py
@@ -4,20 +4,17 @@ try:
     doc = fitz.open(file) 
 except Exception as e:
     print(e)
-page_count  = doc.pageCount
-for page in doc:
-    print(page)
-    #doc2 = fitz.open() 
-    #doc2.insert_pdf(doc,to_page =1)
+#page_count  = doc.pageCount
+for page in doc:   
+    doc2 =fitz.open()
+    doc2.insert_pdf(doc)
+    doc2.save("data/physicsnobel-%i.pdf"%page.number)
     
-
+    
     
     
    
 
 
 
-    #doc2 = fitz.open()                 # new empty PDF
-    #x.insert_pdf(doc, to_page =1)  # first 10 pages
-    #doc2.insert_pdf(doc, from_page = len(doc) - 10) # last 10 pages
-    #doc2.save("k.pdf")
+   
